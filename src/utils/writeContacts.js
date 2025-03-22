cdcd87f6da3eb6ce9b23ba6022f26d453ee3d90f
Bohdan Potopliak <bohdan.potoplyak12345@gmail.com>
2025-03-22 19:35:45 +0200

    finish

--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,5 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = (updateContacts) =>
+  fs.writeFile(PATH_DB, JSON.stringify(updateContacts, null, 2));
